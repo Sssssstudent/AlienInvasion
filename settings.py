33f11caa3898223f4a325426f0530b8b481b62c4
Nastya <iskoskina.nastya@mail.ru>
2022-02-01 23:37:06 +0300

    init

---
+++ settings.py
@@ -0,0 +1,15 @@
+class Settings():
+    """класс для сохранения всех настроек игры Alien Invasion"""
+
+    def __init__(self):
+        """инициализирует настройки  игры"""
+
+        #параметры экрана
+        self.screen_width = 2000
+        self.screen_heigt = 1300
+
+        # назначение цвета фона
+        self.bg_color = (230, 230, 230)
+
+        #скорость перемещения корабля
+        self.ship_speed = 1.5
---
+++ ship.py
@@ -0,0 +1,38 @@
+import pygame
+
+
+class Ship:
+
+    #класс для управления кораблем"""
+    def __init__(self, ai_game):
+        #инициализирует корабль и дает его начальную позицию
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        #загружает изображение корабля и получант прямоугольник
+        self.image = pygame.image.load('images/ship2.bmp')
+        self.rect = self.image.get_rect()
+        #каждый новый корабль появляется у нижнего края экрана
+        self.rect.midbottom = self.screen_rect.midbottom
+        #сохранение вещественной координаты центра корабля
+        self.x = float(self.rect.x)
+
+        #флаг передвижения
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        #обновление позиции корабля с учетом флага.
+        #обновляет атрибут х, не rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #обновление атрибута rect на основании self.x
+        self.rect.x = self.x
+
+    def blitme(self):
+        #рисует корабль в текущей позиции
+        self.screen.blit(self.image, self.rect)
