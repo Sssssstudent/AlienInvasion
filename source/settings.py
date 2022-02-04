5d305dff3b3aef4ab7a513daf4ab9846b0ae6f8d
Nastya <iskoskina.nastya@mail.ru>
2022-02-05 00:47:53 +0300

    - add bullet limit - change bullet color to red - add deletion of old bullets

--- source/settings.py
+++ source/settings.py
@@ -19,4 +19,6 @@ class Settings():
         self.bullet_speed = 1
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = (255, 0, 0)
+        self.bullet_allowed = 5
+
