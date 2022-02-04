a47e527786126ef818c7785c1f594a30e294cbfb
Nastya <iskoskina.nastya@mail.ru>
2022-02-04 23:39:05 +0300

    - add handle  the group of bullets - add handle of press space - create the method to fire a bullet - add drawing of a bullet

--- source/alien_invasion.py
+++ source/alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """класс для управления ресурсами и поведением игры."""
@@ -24,6 +25,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
 
 
@@ -32,6 +34,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -51,6 +54,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
 
@@ -63,13 +68,19 @@ class AlienInvasion:
             self.ship.moving_left = False
 
 
+    def _fire_bullet(self):
+        """ Create a new bullet"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_screen(self):
         #обновляет изображение на экране и отображает новый экран.
         self.screen.blit(self.screen_image, self.screen_rect)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
-            #отображение последнего прорисованного экрана.
+        #отображение последнего прорисованного экрана.
         pygame.display.flip()
 
 
--- source/bullet.py
+++ source/bullet.py
@@ -3,30 +3,30 @@ from pygame.sprite import Sprite
 
 
 class Bullet(Sprite):
-    """Class to manage shells, fired by the ship"""
+    """Class to manage bullets, fired by the ship"""
 
     def __init__(self, ai_game):
-        """Create a shell instance at the ship position"""
+        """Create a bullet instance at the ship position"""
         super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
 
-        # Create a shell at (0,0) position and move it to correct position
+        # Create a bullet at (0,0) position and move it to correct position
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
                                 self.settings.bullet_height)
         self.rect.midtop = ai_game.ship.rect.midtop
 
-        # The shell position is stored in float format
+        # The bullet position is stored in float format
         self.y = self.rect.y
 
     def update(self):
-        """ Move a shell up the screen """
-        # update the shell position using float format
+        """ Move a bullet up the screen """
+        # update the bullet position using float format
         self.y -= self.settings.bullet_speed
-        # update the rect position of the shell
+        # update the rect position of the bullet
         self.rect.y = self.y
 
     def draw_bullet(self):
-        """ Draw a shell on the screen """
+        """ Draw a bullet on the screen """
         pygame.draw.rect(self.screen, self.color, self.rect)
