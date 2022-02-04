73d1eb45a952dfb48b2692c9be6a5199d1d46d00
Nastya <iskoskina.nastya@mail.ru>
2022-02-04 20:32:25 +0300

    создан новый класс Bullet добавлена стрельба в AlienInvasion добавлены атрибуты в Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from SuperHeroes import SuperHeroes
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -20,6 +21,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
+        self.bullets = pygame.sprite.Group()
 
         #Надпись окна
         pygame.display.set_caption("Alien Invasion")
@@ -34,7 +36,18 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
+
+    def _update_bullets(self):
+        #обновляет позиции снарядов и уничтожает старые снаряды
+        #обновление позиций снарядов
+        self.bullets.update()
+
+        #удаление снарядов, вышедших за край экрана
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _check_events(self):
         #обрабатывает нажатия клавиш и события мыши.
@@ -55,6 +68,9 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
 
 
     def _check_keyup_events(self, event):
@@ -65,12 +81,20 @@ class AlienInvasion:
             self.ship.moving_left = False
 
 
+    def _fire_bullet(self):
+        #создание нового снаряда и включение его в группу bullets
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
+
 
     def _update_screen(self):
         #обновляет изображение на экране и отображает новый экран.
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
         self.hero.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
             #отображение последнего прорисованного экрана.
         pygame.display.flip()
--- settings.py
+++ settings.py
@@ -13,3 +13,9 @@ class Settings():
 
         #скорость перемещения корабля
         self.ship_speed = 1.5
+
+        #параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
